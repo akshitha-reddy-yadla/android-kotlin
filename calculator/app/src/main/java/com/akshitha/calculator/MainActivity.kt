2e62a13d7fd3f6836054ae9f8d538cd6f58ee0dc
yadla-akshitha <akshithareddy.yadla@gmail.com>
2025-02-26 12:54:49 +0530

    Write functions to perform activity

--- calculator/app/src/main/java/com/akshitha/calculator/MainActivity.kt
+++ calculator/app/src/main/java/com/akshitha/calculator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.akshitha.calculator
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,17 +10,137 @@ import com.akshitha.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mainBinding: ActivityMainBinding
+    private lateinit var mainBinding: ActivityMainBinding
+    private var number: String? = null
+
+    private var firstNumber: Double = 0.0;
+    private var lastNumber: Double = 0.0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
         val view = mainBinding.root
         setContentView(view)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        mainBinding.textViewResult.text = "0"
+
+        mainBinding.btnZero.setOnClickListener {
+            onNumberClicked("0")
+        }
+
+        mainBinding.btnOne.setOnClickListener {
+            onNumberClicked("1")
+        }
+
+        mainBinding.btnTwo.setOnClickListener {
+            onNumberClicked("2")
+        }
+
+        mainBinding.btnThree.setOnClickListener {
+            onNumberClicked("3")
+        }
+
+        mainBinding.btnFour.setOnClickListener {
+            onNumberClicked("4")
+        }
+
+        mainBinding.btnFive.setOnClickListener {
+            onNumberClicked("5")
+        }
+
+        mainBinding.btnSix.setOnClickListener {
+            onNumberClicked("6")
+        }
+
+        mainBinding.btnSeven.setOnClickListener {
+            onNumberClicked("7")
+        }
+
+        mainBinding.btnEight.setOnClickListener {
+            onNumberClicked("8")
+        }
+
+        mainBinding.btnNine.setOnClickListener {
+            onNumberClicked("9")
+        }
+
+        mainBinding.btnAc.setOnClickListener {
+
+        }
+
+        mainBinding.btnDel.setOnClickListener {
+
+        }
+
+        mainBinding.btnDivide.setOnClickListener {
+            
+        }
+
+        mainBinding.btnMultiply.setOnClickListener {
+
+        }
+        mainBinding.btnMinus.setOnClickListener {
+
+        }
+
+        mainBinding.btnPlus.setOnClickListener {
+
+        }
+
+        mainBinding.btnEqual.setOnClickListener {
+
+        }
+
+        mainBinding.btnDot.setOnClickListener {
+
+        }
     }
+
+    private fun onNumberClicked(clickedNumber: String) {
+        if (number == null) {
+            number = clickedNumber
+        } else {
+            number += clickedNumber
+        }
+        mainBinding.textViewResult.text = number
+    }
+
+    fun plus() {
+        lastNumber = mainBinding.textViewResult.text.toString().toDouble()
+        firstNumber += lastNumber
+        mainBinding.textViewResult.text = firstNumber.toString()
+    }
+
+    fun minus() {
+        lastNumber = mainBinding.textViewResult.text.toString().toDouble()
+        firstNumber -= lastNumber
+        mainBinding.textViewResult.text = firstNumber.toString()
+    }
+
+    fun multiply() {
+        lastNumber = mainBinding.textViewResult.text.toString().toDouble()
+        firstNumber *= lastNumber
+        mainBinding.textViewResult.text = firstNumber.toString()
+    }
+
+    fun divide() {
+        lastNumber = mainBinding.textViewResult.text.toString().toDouble()
+
+        if (lastNumber == 0.0) {
+            Toast.makeText(applicationContext, "The divisor cannot be zero", Toast.LENGTH_LONG)
+                .show()
+        } else {
+            firstNumber /= lastNumber
+            mainBinding.textViewResult.text = firstNumber.toString()
+        }
+    }
+
+
 }
