47b72c6be4fd826e6b920ed83ed458a0a470bb96
yadla-akshitha <akshithareddy.yadla@gmail.com>
2025-02-27 12:14:03 +0530

    Change Theme

--- calculator/app/src/main/java/com/akshitha/calculator/ChangeThemeActivity.kt
+++ calculator/app/src/main/java/com/akshitha/calculator/ChangeThemeActivity.kt
@@ -1,20 +1,65 @@
 package com.akshitha.calculator
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.widget.CompoundButton
+import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.akshitha.calculator.databinding.ActivityChangeThemeBinding
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class ChangeThemeActivity : AppCompatActivity() {
+
+    lateinit var switchBinding: ActivityChangeThemeBinding
+
+    lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        switchBinding = ActivityChangeThemeBinding.inflate(layoutInflater)
+        val view = switchBinding.root
         setContentView(R.layout.activity_change_theme)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        switchBinding.toolbar2.setNavigationOnClickListener {
+            finish()
+        }
+
+        val onOffSwitch: SwitchMaterial = findViewById(R.id.mySwitch)
+
+        val function: (CompoundButton, Boolean) -> Unit = { _, isChecked ->
+
+            sharedPreferences = this.getSharedPreferences("Dark Theme", MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putBoolean("switch", true)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putBoolean("switch", false)
+            }
+            editor.apply()
+        }
+        onOffSwitch.setOnCheckedChangeListener(function)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sharedPreferences = this.getSharedPreferences("Dark Theme", Context.MODE_PRIVATE)
+
+        val isDark = sharedPreferences.getBoolean("switch", false)
+        switchBinding.mySwitch.isChecked = isDark
     }
 }
