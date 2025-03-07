f1999bbcc5f239d5b54acdd260f4dc7bf917b078
yadla-akshitha <akshithareddy.yadla@gmail.com>
2025-03-07 12:44:56 +0530

    Result page design

--- FlagQuizGame/app/build.gradle.kts
+++ FlagQuizGame/app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- FlagQuizGame/app/src/main/java/com/akshitha/flagquizgame/view/FragmentResult.kt
+++ FlagQuizGame/app/src/main/java/com/akshitha/flagquizgame/view/FragmentResult.kt
@@ -6,14 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.akshitha.flagquizgame.R
+import com.akshitha.flagquizgame.databinding.FragmentResultBinding
 
 class FragmentResult : Fragment() {
+
+    private lateinit var fragementResultBinding: FragmentResultBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
-    }
+        fragementResultBinding = FragmentResultBinding.inflate(inflater, container, false)
 
+        fragementResultBinding.btnNewQuiz.setOnClickListener {  }
+
+        fragementResultBinding.btnExit.setOnClickListener {  }
+
+        return fragementResultBinding.root
+    }
 }
